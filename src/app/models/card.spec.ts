888474827932ddb164260a00564318a7d1e346ec
timdvlpr <81222680+timdvlpr@users.noreply.github.com>
2022-03-06 15:15:19 +0100

    card and deck model tests added

--- src/app/models/card.spec.ts
+++ src/app/models/card.spec.ts
@@ -4,4 +4,17 @@ describe('Card', () => {
   it('should create an instance', () => {
     expect(new Card('♥', '10')).toBeTruthy();
   });
+
+  it('should set value depending on card type', () => {
+    const king = new Card('♥', 'K');
+    const queen = new Card('♥', 'Q');
+    const jack = new Card('♥', 'J');
+    const ace = new Card('♥', 'A');
+    const five = new Card('♥', '5');
+    expect(king.value).toBe(10);
+    expect(queen.value).toBe(10);
+    expect(jack.value).toBe(10);
+    expect(ace.value).toBe(11);
+    expect(five.value).toBe(5);
+  });
 });
--- src/app/models/deck.spec.ts
+++ src/app/models/deck.spec.ts
@@ -4,4 +4,37 @@ describe('Deck', () => {
   it('should create an instance', () => {
     expect(new Deck()).toBeTruthy();
   });
+
+  it('should initialize deck with 52 cards', () => {
+    expect(new Deck().cards.length).toBe(52);
+  });
+
+  it('should initialize deck correctly with all card variations', () => {
+    const suites = ['♥', '♦', '♣', '♠'];
+    const types = [
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8',
+      '9',
+      '10',
+      'J',
+      'Q',
+      'K',
+      'A'
+    ];
+    const deck = new Deck();
+
+    suites.forEach((suit) => {
+      types.forEach((type) => {
+        const card = deck.cards.find(
+          (card) => card.suit === suit && card.type === type
+        );
+        expect(card).not.toBeUndefined();
+      });
+    });
+  });
 });
