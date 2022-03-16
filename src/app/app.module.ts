b6072c3a67366ac4c914546063b0f559cbc68def
timdvlpr <81222680+timdvlpr@users.noreply.github.com>
2022-03-16 17:21:28 +0100

    app module updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './components/card/card.component';
+import { PlayerComponent } from './components/player/player.component';
+import { DealerComponent } from './components/dealer/dealer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    PlayerComponent,
+    DealerComponent
   ],
   imports: [
     BrowserModule
