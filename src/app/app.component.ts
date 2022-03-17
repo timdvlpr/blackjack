970ddce4e29d5a91b325f2d739c6207b8461ece3
timdvlpr <81222680+timdvlpr@users.noreply.github.com>
2022-03-17 09:48:22 +0100

    initializing of game added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Hand } from './models/hand';
+import { DeckService } from './services/deck.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+  dealerHand: Hand = new Hand([]);
+  playerHands: Hand[] = [new Hand([])];
+  currentHandIndex = 0;
+
+  constructor(private deckService: DeckService) {}
+
+  initGame(): void {
+    this.deckService.initDeckOfCards(6);
+    for (let i = 1; i < 5; i++) {
+      if (i % 2 === 0) {
+        this.playerHands[0].addCard(this.deckService.nextCard!, true);
+      } else {
+        this.dealerHand.addCard(this.deckService.nextCard!, false);
+      }
+    }
+  }
+
+  ngOnInit(): void {
+    this.initGame();
+  }
+}
