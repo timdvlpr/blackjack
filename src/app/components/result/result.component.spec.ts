6e9b174aae1d25d988fdd3fa8b80b66f2ce0f559
timdvlpr <81222680+timdvlpr@users.noreply.github.com>
2022-04-03 16:42:45 +0200

    result component tests added

--- src/app/components/result/result.component.spec.ts
+++ src/app/components/result/result.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ResultComponent } from './result.component';
+import { Result } from '../../enums/result';
 
 describe('ResultComponent', () => {
   let component: ResultComponent;
@@ -8,9 +9,8 @@ describe('ResultComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ResultComponent ]
-    })
-    .compileComponents();
+      declarations: [ResultComponent]
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +21,33 @@ describe('ResultComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render win message if player won', () => {
+    component.result = Result.WIN;
+    fixture.detectChanges();
+    const msg = fixture.debugElement.nativeElement.querySelector('p');
+    expect(msg.innerText).toBe('WIN');
+  });
+
+  it('should render loss message if player lost', () => {
+    component.result = Result.LOSS;
+    fixture.detectChanges();
+    const msg = fixture.debugElement.nativeElement.querySelector('p');
+    expect(msg.innerText).toBe('LOSS');
+  });
+
+  it('should render tie message if player tied', () => {
+    component.result = Result.TIE;
+    fixture.detectChanges();
+    const msg = fixture.debugElement.nativeElement.querySelector('p');
+    expect(msg.innerText).toBe('TIE');
+  });
+
+  it('should render bust message if player busted', () => {
+    component.result = Result.BUST;
+    fixture.detectChanges();
+    const msg = fixture.debugElement.nativeElement.querySelector('p');
+    expect(msg.innerText).toBe('BUST');
   });
 });
