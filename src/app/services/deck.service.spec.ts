2fd953f4de0f195b0ba42b76bad66fcdca15f0ac
timdvlpr <81222680+timdvlpr@users.noreply.github.com>
2022-04-03 18:02:51 +0200

    deck service tests added

--- src/app/services/deck.service.spec.ts
+++ src/app/services/deck.service.spec.ts
@@ -13,4 +13,23 @@ describe('DeckService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should init deck of cards correctly', () => {
+    service.initDeckOfCards(1);
+    expect(service.deckCards.length).toBe(52);
+    service.initDeckOfCards(3);
+    expect(service.deckCards.length).toBe(156);
+    service.initDeckOfCards(6);
+    expect(service.deckCards.length).toBe(312);
+  });
+
+  it('should get next card from deck', () => {
+    service.initDeckOfCards(6);
+    expect(service.nextCard).not.toBeNull();
+  });
+
+  it('should return null if no card is left', () => {
+    expect(service.deckCards.length).toBe(0);
+    expect(service.nextCard).toBeNull();
+  });
 });
