d56ad6747f161bd5fad4fc088dcba0a39923ba98
timdvlpr <81222680+timdvlpr@users.noreply.github.com>
2022-04-03 16:07:52 +0200

    button tests added

--- src/app/components/action-button/action-button.component.spec.ts
+++ src/app/components/action-button/action-button.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ActionButtonComponent } from './action-button.component';
+import { IconModule } from '../../icon.module';
 
 describe('ActionButtonComponent', () => {
   let component: ActionButtonComponent;
@@ -8,9 +9,9 @@ describe('ActionButtonComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ActionButtonComponent ]
-    })
-    .compileComponents();
+      declarations: [ActionButtonComponent],
+      imports: [IconModule]
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +22,36 @@ describe('ActionButtonComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render hit button if type is hit', () => {
+    component.type = 'hit';
+    fixture.detectChanges();
+    const hitBtn = fixture.debugElement.nativeElement.querySelector('.hit-btn');
+    expect(hitBtn).not.toBeNull();
+  });
+
+  it('should render stand button if type is stand', () => {
+    component.type = 'stand';
+    fixture.detectChanges();
+    const standBtn =
+      fixture.debugElement.nativeElement.querySelector('.stand-btn');
+    expect(standBtn).not.toBeNull();
+  });
+
+  it('should render split button if type is split', () => {
+    component.type = 'split';
+    fixture.detectChanges();
+    const splitBtn =
+      fixture.debugElement.nativeElement.querySelector('.split-btn');
+    expect(splitBtn).not.toBeNull();
+  });
+
+  it('should render new round button if type is newRound', () => {
+    component.type = 'newRound';
+    fixture.detectChanges();
+    const newRoundBtn =
+      fixture.debugElement.nativeElement.querySelector('.new-round-btn');
+    expect(newRoundBtn).not.toBeNull();
   });
 });
